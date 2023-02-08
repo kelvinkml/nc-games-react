8a75a9ed27bf14c8da73c28682d554782024040a
kelvinkml <kelvinkml@live.com>
2023-02-08 08:47:18 +0000

    adds get comments

---
+++ nc-games/src/components/Comments.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+import { instance } from "../utils/axios"
+
+export const Comments = ({id}) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [comments, setComments] = useState([])
+    useEffect(()=>{
+        instance.get(`/reviews/${id}/comments`).then((result)=>{
+            setComments(result)
+            setIsLoading(false)
+        })
+    }, [id])
+    if(isLoading){
+        return <p>Comments Loading...</p>
+    }
+    else
+    return (
+        <section>
+            <h4>Comments:</h4>
+            <p>Add Comment...</p>
+            {comments.data.comments.map((comment)=>{
+                return (
+                <section key={comment.comment_id}>
+                    <p>{comment.author} says: </p>
+                    <p>{comment.body}</p>
+                </section>
+                )
+            })}
+        </section>
+    
+    )
+}
