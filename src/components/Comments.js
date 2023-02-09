316f3e43081d9ff44b86841199f24a05a0bed2a4
kelvinkml <kelvinkml@live.com>
2023-02-09 11:08:31 +0000

    adds post comment and basic hard-coded user state

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,15 +1,35 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 import { instance } from "../utils/axios"
+import { UserContext } from "../contexts/UserContext"
 
 export const Comments = ({id}) => {
+    const { user } = useContext(UserContext)
+
+    const [comment, setComment] = useState()
+    const [refresh, setRefresh] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
     const [comments, setComments] = useState([])
+    
     useEffect(()=>{
         instance.get(`/reviews/${id}/comments`).then((result)=>{
-            setComments(result)
             setIsLoading(false)
+            setComments(result.data.comments)
         })
-    }, [id])
+    }, [id, refresh])
+    const newComment = (event) => {
+        event.preventDefault()
+        const commentToPost = {
+            body: comment,
+            username: user
+        }
+        instance.post(`/reviews/${id}/comments`, commentToPost).then((result)=>{
+        setRefresh(true)
+        setIsLoading(true)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
     if(isLoading){
         return <p>Comments Loading...</p>
     }
@@ -17,8 +37,18 @@ export const Comments = ({id}) => {
     return (
         <section>
             <h4>Comments:</h4>
-            <p>Add Comment...</p>
-            {comments.data.comments.map((comment)=>{
+            <form>
+                <label htmlFor="comment">New Comment</label><br></br>
+                <input 
+                type='text'
+                id='comment'
+                onChange={(event)=>
+                setComment(event.target.value)}
+                >                
+                </input>
+                <button onClick={newComment}>Submit</button>
+            </form><br></br>
+            {comments.map((comment)=>{
                 return (
                 <section key={comment.comment_id}>
                     <p>{comment.author} says: </p>
